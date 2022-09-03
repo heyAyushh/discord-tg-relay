9843982dab596fb2c8fda5223731e88a8ef6326f
Ayush <ayush-15@live.com>
2022-09-03 17:03:00 +0530

    updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import { Message, Client, Intents } from 'discord.js';
+import { Message, Client, IntentsBitField } from 'discord.js';
 import 'dotenv/config';
 import { Telegraf } from 'telegraf';
 import logger from './utils/logger';
 
 if (process.env.TELEGRAM_BOT_TOKEN === undefined) {
  throw new TypeError('BOT_TOKEN must be provided!');
-} else if (!process.env.TELEGRAM_CHANNEL_ID) {
+} if (!process.env.TELEGRAM_CHANNEL_ID) {
  throw new TypeError('TELEGRAM_CHANNEL_ID must be provided!');
 }
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN as string);
 
 const client = new Client({
- intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
 });
+
+if (process.env.DISCORD_BOT_TOKEN === undefined) {
+ throw new TypeError('DISCORD_BOT_TOKEN must be provided!');
+} if (!process.env.DISCORD_CHANNEL_ID) {
+ throw new TypeError('DISCORD_CHANNEL_ID must be provided!');
+}
 
 client.login(process.env.DISCORD_BOT_TOKEN as string);
 
